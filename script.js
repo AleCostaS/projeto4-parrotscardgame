d4cf3464c684dec991f18833d9bb91f864267824
AleCostaS <AleCostaS@github.com>
2022-06-17 18:05:02 -0300

    as cartas já viram

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let videos = ["imagens/unicornparrot.gif", "imagens/tripletsparrot.gif", "imagens/revertitparrot.gif", "imagens/metalparrot.gif", "imagens/fiestaparrot.gif", "imagens/explodyparrot.gif", "imagens/bobrossparrot.gif"];
 let numerodecartas = 0;
+let c = 0;
+let j = 0;
 
 while (((numerodecartas % 2 == 0) && numerodecartas >= 4 && numerodecartas <= 14) == false){
     numerodecartas = prompt("Quantas cartas você quer jogar? (escolha um número par de 4 a 14)");
@@ -6,29 +9,32 @@ while (((numerodecartas % 2 == 0) && numerodecartas >= 4 && numerodecartas <= 14
 
 let cartas = [];
 
-for (let i = 1; i <= numerodecartas; i++){
+for (let i = 0; i < numerodecartas; i++){
     cartas.push(i);
 }
-
-console.log(cartas);
-
 cartas.sort(comparador);
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-
 const jogo = document.querySelector(".jogo");
 
-for (let i = 1; i <= numerodecartas; i++){
-    jogo.innerHTML += `<span class="card">
+for (let i = 0; i < numerodecartas; i++){
+    jogo.innerHTML += `<div class="card" onclick="virar(this)">
             <div class="front-face face">
-            <img src="imagens/front.png"></img>
+                <img src="imagens/front.png"></img>
             </div>
             <div class="back-face face">
-            ${cartas[i]}
+                <img src="${videos[Math.floor(cartas[i]/2)]}">
             </div>
-        </span>`;
+        </div>`;
 }
 
+function virar(elemento){
+    const virando = document.querySelector(".virando");
+    if (virando != null){
+
+    }
+    elemento.classList.add("virando")
+} 
