c0ecee5b176722dcb818811ee2a39d1c9f6e746c
AleCostaS <AleCostaS@github.com>
2022-06-17 13:47:07 -0300

    Cartas criadas e embaralhadas com layout responsivo

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-let cartas = 0;
+let numerodecartas = 0;
 
-while (((cartas % 2 == 0) && cartas >= 4 && cartas <= 14) == false){
-    cartas = prompt("Quantas cartas você quer jogar? (escolha um número par de 4 a 14)");
+while (((numerodecartas % 2 == 0) && numerodecartas >= 4 && numerodecartas <= 14) == false){
+    numerodecartas = prompt("Quantas cartas você quer jogar? (escolha um número par de 4 a 14)");
 }
+
+let cartas = [];
+
+for (let i = 1; i <= numerodecartas; i++){
+    cartas.push(i);
+}
+
+console.log(cartas);
+
+cartas.sort(comparador);
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+const jogo = document.querySelector(".jogo");
+
+for (let i = 1; i <= numerodecartas; i++){
+    jogo.innerHTML += `<span class="card">
+            <div class="front-face face">
+            <img src="imagens/front.png"></img>
+            </div>
+            <div class="back-face face">
+            ${cartas[i]}
+            </div>
+        </span>`;
+}
+
